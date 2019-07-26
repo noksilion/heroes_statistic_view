4489165c3f6e4b9fc4b5f7bbb7e91025e03df0a6
andrey.kasianov <noksilion@gmail.com>
2019-07-26 23:05:42 +0300

    get data from rest server by cross domain request

---
+++ src/main/resources/static/js/test.js
@@ -0,0 +1,13 @@
+function test() {
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.open("GET", "http://localhost:8080/castles/get_all", true);
+    xhr.responseType = "json";
+    xhr.onload = function() {
+        console.log(xhr.response);
+    };
+    xhr.send();
+
+}
